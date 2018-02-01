fca067322ae934304faa05c06a1130b17787beb2
K Sichien <ksichien@posteo.net>
2018-02-01 15:53:25 +0100

    revised aerial downloader

--- aerialdownloader.py
+++ aerialdownloader.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script will download the latest Apple TV Aerial screensavers.
+This script will download the Apple TV Aerial videos to an 'Aerial' folder in the home directory.
 Inspired by https://gist.github.com/Dhertz/9dd69eaad092d0c0fe96
 """
 import json
@@ -12,19 +12,20 @@ DLDIR = os.getenv("HOME") + '/Aerial/'
 if not os.path.exists(DLDIR):
     os.makedirs(DLDIR)
 
-RSP = requests.get('http://a1.phobos.apple.com/us/r1000/000/Features/atv/' +
-                   'AutumnResources/videos/entries.json')
+RSP = requests.get('http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/' +
+                   'videos/entries.json')
 
 ARL = json.loads(RSP.text)
 for aerial in ARL:
     for asset in aerial['assets']:
-        filename = asset['url'].split('/')
-        filepath = DLDIR + filename[-1]
+        filename = (asset['accessibilityLabel'].replace(" ", "-") + '-' +
+                    asset['timeOfDay'] + '-' + asset['id'] + '.mov')
+        filepath = DLDIR + filename
         if not os.path.isfile(filepath):
             print("Downloading %s" % (asset['url'],))
             video = requests.get(asset['url'], stream=True)
             with open(filepath, "wb") as videoFile:
-                print("Writing %s to %s" % (asset['id'], filepath))
+                print("Writing %s to %s" % (filename, DLDIR))
                 for chunk in video.iter_content(chunk_size=1024):
                     if chunk:
                         videoFile.write(chunk)
